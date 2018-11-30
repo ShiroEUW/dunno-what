c021ad478bd6609aeefc1e008f221a1a1b9a5094
givemeyourmoneypls <45392169+ShiroEUW@users.noreply.github.com>
2018-11-30 08:33:05 +0100

    Add files via upload

---
+++ Projekt einfacher Taschenrechner.py
@@ -0,0 +1,30 @@
+x = int(input("Erste Zahl: "))
+
+y = int(input("Zweite Zahl: "))
+
+z = input("Welches Rechnenzeichen möchten Sie verwenden: ")
+
+if z == "+":
+    
+    print("Das Ergebnis aus", int(x), " + ", int(y))
+    print("ist gleich", int(x) + int(y), "!")
+    
+elif z == "-":
+    
+    print("Das Ergebnis aus", int(x), " - ", int(y))
+    print("ist gleich", int(x) - int(y), "!")
+    
+    
+elif z == "/":
+    
+    print("Das Ergebnis aus", int(x), " / ", int(y))
+    print("ist gleich", int(x) / int(y), "!")
+    
+elif z == "*":
+    
+    print("Das Ergebnis aus", int(x), " * ", int(y))
+    print("ist gleich", int(x) * int(y), "!")
+    
+else:
+    print(z, "kenne ich nicht")
+
